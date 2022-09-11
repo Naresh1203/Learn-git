240918af8bb350c93196ba48029b1dee199f4255
Naresh <officialnaresh1103@gmail.com>
2022-09-11 08:59:48 +0530

    age increase by 30

--- age.c
+++ age.c
@@ -4,7 +4,7 @@ int main()
     int age;
     
     scanf("%d",&age);
-    printf("Age of a person after 15 years = %d",age+15);
+    printf("Age of a person after 15 years = %d",age+30);
     return 0;
 
 }
